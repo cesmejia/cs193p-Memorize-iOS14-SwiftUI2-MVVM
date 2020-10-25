67a9fcc4e2632a98e69e6bfaa0a6cf68da29eb42
Ces <cesmejia@gruposeara.com>
2020-10-25 07:48:44 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ces Mejia on 25/10/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
